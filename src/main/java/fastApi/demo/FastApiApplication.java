dd66b7fef44c10b99d87d633f41374dc30808295
Ruisu Eurimonio <pipelinares2912@gmail.com>
2024-08-17 16:44:33 -0500

    Initial commit with the running application setup.

---
+++ src/main/java/fastApi/demo/FastApiApplication.java
@@ -0,0 +1,18 @@
+package fastApi.demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastApiApplication {
+
+        private final static Logger logger = LogManager.getLogger(FastApiApplication.class);
+    
+	public static void main(String[] args) {
+            SpringApplication.run(FastApiApplication.class, args);
+            logger.info("Ejecutando programa.");
+	}
+
+}
---
+++ src/test/java/fastApi/demo/FastApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fastApi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
